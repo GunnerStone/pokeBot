a34f9509a374e9eb67e625650ea25a445fad6fc6
coopflourens <64337055+coopflourens@users.noreply.github.com>
2021-02-18 01:56:46 -0800

    Added Twilio support

---
+++ pokeTwilio.py
@@ -0,0 +1,24 @@
+from twilio.rest import Client
+import config
+
+#   This file holds a function to call a player with a statement. In this case, finding a shiny.
+#
+#   Setup:
+#   Create a config.py folder that includes the following varibles:
+#   to_phone_number = 'your number'
+#   from_phone_number = 'Twilio number'
+#   account_sid = 'from Twilio'
+#   auth_token = 'from Twilio'
+#
+#   Have fun and happy hunting!
+#   Cooper Flourens
+
+def found_shiny_call(to_num = config.to_phone_number, from_num = config.from_phone_number): 
+    # This function calls a user and says the message "You Found a Shiny!". Usage: found_shiny_call(to_num, from_num). Num format: Country Code + Area Code + Number (example: '+12223333333')
+    account_sid = config.account_sid
+    auth_token = config.auth_token
+    client = Client(account_sid, auth_token)
+    client.calls.create(twiml='<Response><Say>You Found a Shiny!</Say></Response>', to = to_num, from_ = from_num)
+
+# remove this to test function call:  
+found_shiny_call()
